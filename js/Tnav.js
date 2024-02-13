52fb6160f62f27a30a7b730b4beff3600b0b3524
Gamerboss3094 <159937293+Gamerboss3094@users.noreply.github.com>
2024-02-13 10:44:28 -0800

    Update Tnav.js

--- js/Tnav.js
+++ js/Tnav.js
@@ -2,7 +2,7 @@ var embed = document.getElementById("embed");
 
 document.addEventListener('click', function(e) {
   var elem = e.target;
-  var isBtn = elem.classList.contains(".Tbtn");
-  var isAct = elem.classList.contains(".Tact");
+  var isBtn = elem.classList.contains("Tbtn");
+  var isAct = elem.classList.contains("Tact");
   alert(isBtn + " " + isAct);
 });
