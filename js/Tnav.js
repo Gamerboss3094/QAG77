84c341e014a54086029059b7f020023c3d05beb8
Gamerboss3094 <159937293+Gamerboss3094@users.noreply.github.com>
2024-02-14 11:39:22 -0800

    Update navigation bar

--- js/Tnav.js
+++ js/Tnav.js
@@ -1,18 +1,18 @@
-var embed = document.getElementById("embed");
+var embed = document.getElementById('embed');
 
 function enable(elem) {
   [...document.getElementsByClassName('Tact')].forEach(x => {
-    x.classList.remove('Tact');
+    x.classList.replace('Tact', 'Tin');
   });
-  elem.classList.add('Tact');
+  elem.classList.replace('Tin', 'Tact');
 }
 
 document.addEventListener('click', function(e) {
   var elem = e.target;
-  var isBtn = elem.classList.contains("Tbtn");
-  var isAct = elem.classList.contains("Tact");
-  if(isBtn === true && isAct === false) {
+  var isBtn = elem.classList.contains('Tbtn');
+  var isAct = elem.classList.contains('Tact');
+  if(isBtn && !isAct) {
     embed.src = elem.name;
-    enable(elem)
+    enable(elem);
   }
 });
