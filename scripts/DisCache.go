0c1c15f016fe6a321cd773e45f91125306303098
Gamerboss3094 <159937293+Gamerboss3094@users.noreply.github.com>
2024-02-14 12:28:51 -0800

    Disable page caching

---
+++ scripts/DisCache.go
@@ -0,0 +1,8 @@
+func DisableCachingAndSniffing(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "no-cache, no-store, must-revalidate;")
+		w.Header().Set("pragma", "no-cache")
+		w.Header().Set("X-Content-Type-Options", "nosniff")
+		next.ServeHTTP(w, r)
+	})
+}
