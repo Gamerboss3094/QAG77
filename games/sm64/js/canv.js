bb018b89ba01b90df425c17c70f5ce4a43200d7a
Kris <159937293+Gamerboss3094@users.noreply.github.com>
2024-02-15 12:17:39 -0800

    Add sm64

---
+++ games/sm64/js/canv.js
@@ -0,0 +1,26 @@
+var Module = {
+  preRun: [],
+  postRun: [],
+  print: (function() {
+    return function(text) {
+      if (arguments.length > 1) text = Array.prototype.slice.call(arguments).join(' ');
+      console.log(text);
+    };
+  })(),
+  printErr: function(text) {
+    if (arguments.length > 1)
+      text = Array.prototype.slice.call(arguments).join(' ');
+    console.error(text);
+  },
+  canvas: (function() {
+    var canvas = document.getElementById('canvas');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    canvas.addEventListener("webglcontextlost", function (e) {
+      alert('WebGL context lost. You will need to reload the page.');
+      e.preventDefault();
+    }, false);
+    return canvas;
+  })(),
+  setStatus: function(text) { }
+};
