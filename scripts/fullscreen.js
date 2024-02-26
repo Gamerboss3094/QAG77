fde91d7390f90fa786df05ca1e5a2c47c95a1406
Kris <159937293+Gamerboss3094@users.noreply.github.com>
2024-02-26 09:18:25 -0800

    undo stuff

--- scripts/fullscreen.js
+++
@@ -1,9 +0,0 @@
-var FSbtn = document.getElementById("FSbtn");
-var embed = document.getElementById('embed');
-
-function fullscreen() {
-  embed.requestFullscreen();
-}
-
-FSbtn.addEventListener("click", fullscreen, false);
-document.addEventListener('fullscreenchange', fullscreen, false);
