75fad4bec50b825aca6d95aa125c1f56fa35d55a
Kris <159937293+Gamerboss3094@users.noreply.github.com>
2024-02-26 11:21:26 -0800

    Add fullscreen stuff

--- scripts/fullscreen.js
+++ scripts/fullscreen.js
@@ -2,6 +2,7 @@ var FSbtn = document.getElementById("FSbtn");
 var embed = document.getElementById("embed");
 
 FSbtn.addEventListener('click', enterFullscreen);
+document.addEventListener('fullscreenchange', enterFullscreen)
 
 function enterFullscreen() {
   embed.requestFullscreen();
